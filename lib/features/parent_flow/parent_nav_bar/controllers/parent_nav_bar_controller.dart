a27ec4bfc412a1e7b6703b3b3d5b888b8f67cbd2
Nifat130 <mahingreen130@gmail.com>
2025-08-25 00:11:11 +0600

    routing done by Nifat

--- lib/features/parent_flow/parent_nav_bar/controllers/parent_nav_bar_controller.dart
+++ lib/features/parent_flow/parent_nav_bar/controllers/parent_nav_bar_controller.dart
@@ -1,8 +1,4 @@
-import 'package:baby_vax/features/hospital_flow/hospital_event/presentation/screens/hospital_event_screen.dart';
-import 'package:baby_vax/features/hospital_flow/hospital_home/presentation/screens/hospital_home_screen.dart';
-import 'package:baby_vax/features/hospital_flow/hospital_profile/presentation/screens/hospital_profile_screen.dart';
 import 'package:baby_vax/features/parent_flow/parent_home_screen/presentation/screens/parent_home_screen.dart';
-import 'package:baby_vax/features/parent_flow/parent_profile/presentation/screens/parent_information_screen.dart';
 import 'package:baby_vax/features/parent_flow/parent_profile/presentation/screens/parent_profile_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
