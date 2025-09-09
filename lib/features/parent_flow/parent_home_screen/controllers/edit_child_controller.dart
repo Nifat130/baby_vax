2d18f3035e101c5f4950f1b908f6151faba2c44c
Nifat130 <mahingreen130@gmail.com>
2025-09-09 23:33:51 +0600

    update child information done by Nifat

--- lib/features/parent_flow/parent_home_screen/controllers/edit_child_controller.dart
+++ lib/features/parent_flow/parent_home_screen/controllers/edit_child_controller.dart
@@ -1,12 +1,19 @@
 
 
+import 'package:baby_vax/core/common/app_snackber.dart';
 import 'package:baby_vax/core/common/widgets/custom_picker_theme.dart';
+import 'package:baby_vax/core/common/widgets/progress_indicator.dart';
+import 'package:baby_vax/core/utils/logging/logger.dart';
 import 'package:baby_vax/data/parent_flow/get_my_children_model.dart';
+import 'package:baby_vax/features/parent_flow/parent_home_screen/controllers/parent_home_controller.dart';
+import 'package:baby_vax/repositories/parent_flow_repositories/parent_repo.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
+
+import '../../../../core/services/Auth_service.dart';
 
 class EditChildController extends GetxController{
 
@@ -25,12 +32,14 @@ class EditChildController extends GetxController{
     if(Get.arguments != null){
       child = Get.arguments;
       childNameTEController.text = child.name!;
+      birthDate = child.birthDate!.toString();
       dateOfBirthTEController.text = DateFormat("dd MMMM, yyyy").format(child.birthDate!.toLocal());
       fartherNameTEController.text = child.fatherName!;
       motherNameTEController.text = child.motherName!;
       birthPlaceTEController.text = child.birthPlace!;
     }
   }
+  var birthDate = "";
   void pickDOB(BuildContext context) async{
     final date = await showDatePicker(
       context: context,
@@ -43,6 +52,7 @@ class EditChildController extends GetxController{
 
     if(date != null){
       dateOfBirthTEController.text = DateFormat("dd MMMM, yyyy").format(date);
+      birthDate = date.toUtc().toIso8601String();
     }
   }
 
@@ -53,6 +63,35 @@ class EditChildController extends GetxController{
 
     if(image != null){
       profileImage.value = image.path;
+    }
+  }
+
+  var parentHomeController = Get.find<ParentHomeController>();
+  var parentRepo = ParentRepo();
+
+  void updateChild() async{
+    final requestBody = {
+      "parent_id": AuthService.id,
+      "name": childNameTEController.text,
+      "birthDate": birthDate,
+      "gender": selectedGender.value,
+      "birthPlace": birthPlaceTEController.text,
+      "profilePicture": child.profilePicture,
+      "fatherName": fartherNameTEController.text,
+      "motherName": motherNameTEController.text,
+      "givenVaccines": child.givenVaccines
+    };
+    AppLoggerHelper.info(requestBody.toString());
+    showProgressIndicator();
+    if(await parentRepo.updateChild(requestBody, child.id!)){
+      await parentHomeController.getMyChildren();
+      Get.back();
+      Get.back();
+      AppSnackBar.showSuccess("Child information updated!!");
+    }
+    else{
+      Get.back();
+      AppSnackBar.showError("Failed to update information!!");
     }
   }
 }
