068b39f43ed75ae991ba45fb526799218997788f
Nifat130 <mahingreen130@gmail.com>
2025-09-16 23:29:49 +0600

    logic reset from parent signup by Nifat

--- lib/features/authentication/presentation/screens/parent_sign_up_screen.dart
+++ lib/features/authentication/presentation/screens/parent_sign_up_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:baby_vax/core/common/app_snackber.dart';
 import 'package:baby_vax/core/utils/constants/app_sizer.dart';
 import 'package:baby_vax/core/utils/constants/image_path.dart';
 import 'package:baby_vax/features/authentication/controllers/sign_up_controller.dart';
@@ -148,8 +149,10 @@ class ParentSignUpScreen extends GetView<SignUpController> {
                   CustomSubmitButton(
                     text: "Create Account",
                     onTap: (){
-                      if(formState.currentState!.validate() || controller.profileImage.value == ''){
-                        controller.createParentAccount();
+                      if(formState.currentState!.validate()){
+                        controller.profileImage.value != '' ?
+                        controller.createParentAccount() :
+                            AppSnackBar.showError("Upload profile picture");
                       }
                     },
                   ),
